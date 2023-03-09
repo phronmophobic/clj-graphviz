c4b615832d129f79ba0584959a6e37a10f64b55e
Adrian Smith <smith.adriane@gmail.com>
2023-03-09 11:02:37 -0800

    Update build tools.

--- build.clj
+++ build.clj
@@ -1,17 +1,45 @@
 (ns build
-  (:refer-clojure :exclude [test])
-  (:require [org.corfield.build :as bb]))
+  (:require [clojure.tools.build.api :as b]
+            [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/clj-graphviz)
 (def version "0.1.0-SNAPSHOT")
-(def main 'com.phronemophobic.clj-graphviz)
 
-(defn test "Run the tests." [opts]
-  (bb/run-tests opts))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
-  (-> opts
-      (assoc :lib lib :version version :main main)
-      (bb/run-tests)
-      (bb/clean)
-      (bb/uber)))
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn compile [_]
+  #_(b/javac {:src-dirs ["src-java"]
+            :class-dir class-dir
+            :basis basis
+            :javac-opts ["-source" "8" "-target" "8"]}))
+
+(defn jar [opts]
+  (compile opts)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy [opts]
+  (jar opts)
+  (try ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+        (merge {:installer :remote
+                :artifact jar-file
+                :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+               opts))
+       (catch Exception e
+         (if-not (str/includes? (ex-message e) "redeploying non-snapshots is not allowed")
+           (throw e)
+           (println "This release was already deployed."))))
+  opts)
+
