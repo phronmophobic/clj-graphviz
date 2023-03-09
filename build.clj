61f2363dc0c6d02d9ccddf8b1243abfd8dd39891
Adrian Smith <smith.adriane@gmail.com>
2023-03-09 14:52:34 -0800

    Bump version to 0.1.0.

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/clj-graphviz)
-(def version "0.1.0-SNAPSHOT")
+(def version "0.1.0")
 
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
