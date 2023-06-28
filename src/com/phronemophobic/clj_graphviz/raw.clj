3366695ead490e681a4350724c8e53d9637db652
Adrian Smith <smith.adriane@gmail.com>
2023-06-28 16:48:44 -0700

    Add debug info.

--- src/com/phronemophobic/clj_graphviz/raw.clj
+++ src/com/phronemophobic/clj_graphviz/raw.clj
@@ -27,7 +27,13 @@
 
 (try
   (require 'com.phronemophobic.cljonda.graphviz)
+
+  (require '[com.phronemophobic.cljonda.core :as cc])
+  (println "system:" (cc/system-arch))
+  (println "path:\n" (System/getProperty "jna.library.path"))
+
   (catch java.io.FileNotFoundException e
+    (println "cljonda graphviz NOT LOADED!")
     nil))
 
 (def ^:no-doc libgvc
